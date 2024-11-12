51f7b51678964821c0deb069c42ac339788433ed
JahnaviMandadi25 <jahnaviapplications@gmail.com>
2024-11-11 20:34:36 -0500

    Myblog

--- DjangoPractice/myapp/views.py
+++ DjangoPractice/myapp/views.py
@@ -4,16 +4,12 @@ from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 def post_list(request):
-    posts = Post.objects.all().order_by('-created_at')  # Descending order by creation date
+    # Retrieve posts ordered by created_at in ascending order (oldest first)
+    posts = Post.objects.all().order_by('created_at')
     return render(request, 'post_list.html', {'posts': posts})
-
-
-
 
 def about_me(request):
     return render(request, 'about_me.html')
-
-
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
